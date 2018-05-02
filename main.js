c8fc9f9cb9e5d00b23de17432fefb2f2a31d7e43
Luke <lukelovemoney@gmail.com>
2018-05-02 16:13:16 +0800

    封装jQuery.ajax

--- main.js
+++ main.js
@@ -1,17 +1,40 @@
-button.addEventListener('click',(e) => {
+window.jQuery.ajax = function({url, method, body, successFn, failFn, headers}){
     let request = new XMLHttpRequest()
-    request.open('post', '/xxx')
-    request.setRequestHeader('content-type', 'x-www-form-url-encoded')
-    request.send('a=1&b=2')
-    request.onreadystatechange = () =>{
-        if(request.readyState === 4){
-            if(request.status >= 200 && request.status < 300){
-                let string = request.responseText
-                console.log(string)
-                let object = window.JSON.parse(string)
-            }else if(request.status > 400){
-                console.log(请求失败)
-            }
-        }
+    request.open(method, url) // 配置request
+    for(let key in headers) {
+      let value = headers[key]
+      request.setRequestHeader(key, value)
     }
-})
+    request.onreadystatechange = ()=>{
+      if(request.readyState === 4){
+        if(request.status >= 200 && request.status < 300){
+          successFn.call(undefined, request.responseText)
+        }else if(request.status >= 400){
+          failFn.call(undefined, request)
+        }
+      }
+    }
+    request.send(body)
+  }
+  
+  
+  
+  myButton.addEventListener('click', (e)=>{
+    window.jQuery.ajax({
+      url: '/xxx',
+      method: 'get',
+      headers: {
+        'content-type':'application/x-www-form-urlencoded',
+        'jack': '18'
+      },
+      successFn: (x)=>{
+        f1.call(undefined,x)
+        f2.call(undefined,x) //callback,成功之后可以执行两个函数
+      },
+      failFn: (x)=>{
+        console.log(x)
+        console.log(x.status)
+        console.log(x.responseText)
+      }
+    })
+  })
