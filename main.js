693927861f5c31432db8c73c7541b88c385dc71f
Luke <lukelovemoney@gmail.com>
2018-05-02 16:21:31 +0800

    把jQuery.ajax变成Promise形式

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
-window.jQuery.ajax = function({url, method, body, successFn, failFn, headers}){
-    let request = new XMLHttpRequest()
-    request.open(method, url) // 配置request
-    for(let key in headers) {
-      let value = headers[key]
-      request.setRequestHeader(key, value)
-    }
-    request.onreadystatechange = ()=>{
-      if(request.readyState === 4){
-        if(request.status >= 200 && request.status < 300){
-          successFn.call(undefined, request.responseText)
-        }else if(request.status >= 400){
-          failFn.call(undefined, request)
+window.jQuery.ajax = function({url, method, body, headers}){
+    return new Promise(function(resolve, reject){
+      let request = new XMLHttpRequest()
+      request.open(method, url) // 配置request
+      for(let key in headers) {
+        let value = headers[key]
+        request.setRequestHeader(key, value)
+      }
+      request.onreadystatechange = ()=>{
+        if(request.readyState === 4){
+          if(request.status >= 200 && request.status < 300){
+            resolve.call(undefined, request.responseText) //成功调用resolve
+          }else if(request.status >= 400){
+            reject.call(undefined, request)  //失败调用reject
+          }
         }
       }
-    }
-    request.send(body)
-  }
-  
-  
+      request.send(body)
+    })
+  } //返回值是一个Promise实例对象，这个实例对象有then属性
   
   myButton.addEventListener('click', (e)=>{
     window.jQuery.ajax({
@@ -26,15 +26,9 @@ window.jQuery.ajax = function({url, method, body, successFn, failFn, headers}){
       headers: {
         'content-type':'application/x-www-form-urlencoded',
         'jack': '18'
-      },
-      successFn: (x)=>{
-        f1.call(undefined,x)
-        f2.call(undefined,x) //callback,成功之后可以执行两个函数
-      },
-      failFn: (x)=>{
-        console.log(x)
-        console.log(x.status)
-        console.log(x.responseText)
       }
-    })
+    }).then(
+      (text)=>{console.log(text)}, //成功时执行第一个参数
+      (request)=>{console.log(request)} //失败时执行第二个参数
+    )
   })
